0db68d1346dbddd0c3329b1521df307c625a5192
WebSEC.nl <rebitclown105@hotmail.com>
2021-07-31 11:19:23 +0200

    Update hashcat.php

--- hashcat.php
+++ hashcat.php
@@ -3,5 +3,6 @@ $wordlist = $_GET['wordlist'];
 $rule = $_GET['rule'];
 $hash = $_GET['hash'];
 $hashcat = system("hashcat -m 1000 '" . escapeshellarg($_GET["hash"]) . "' " . escapeshellarg($_GET["wordlist"]) . " -r " . escapeshellarg($_GET["rule"]) . " -O");
+sleep(5);
 echo $hashcat;
 ?>
