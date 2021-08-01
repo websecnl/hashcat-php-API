2b57f6e89f37203fcd0ec629c5aac85e62917d35
WebSEC.nl <rebitclown105@hotmail.com>
2021-08-01 21:48:46 +0200

    Update hashcat.php

--- hashcat.php
+++ hashcat.php
@@ -5,8 +5,7 @@ $hash = $_GET['hash'];
 
 // Yes yes I know, this is still not safe, but its better than nothing.
 trim($hash,"stdout");
-trim($hash,"-");
-trim($hash,"%2d");
+trim($hash,"%73%74%64%6f%75%74");
 trim($wordlist,"../");
 
 $hashcat = system("hashcat -m 1000 '" . escapeshellarg($_GET["hash"]) . "' " . escapeshellarg($_GET["wordlist"]) . " -r " . escapeshellarg($_GET["rule"]) . " -O");
